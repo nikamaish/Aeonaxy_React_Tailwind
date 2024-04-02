3d746091d26a4259a9ac4ee6737f8c76bd3034a5
aish <aishnikam31@gmail.com>
2024-04-02 23:41:33 +0530

    :package: new dependency

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Interest from '../Interest/Interest';
 import { Link } from 'react-router-dom';
 
 const Description = () => {
@@ -50,7 +49,7 @@ const Description = () => {
             <div className="flex flex-col items-center mt-12">
                 {personalData.map((data, index) => (
                     <div key={data.id} className={`flex items-center border rounded-md  mb-4 w-full p-2 md:w-1/2 text-gray-800 cursor-pointer ${selectedBox === data.id ? 'border-golden' : 'border-gray-300'}`} onClick={() => handleBoxClick(data.id)}>
-                        <img src={data.image} alt="vector image" width={40} height={20} className='mr-5'/>
+                        <img src={data.image} alt="vector" width={40} height={20} className='mr-5'/>
                         {data.title}
                     </div>
                 ))}
--- src/components/Interest/Interest.jsx
+++ src/components/Interest/Interest.jsx
@@ -45,7 +45,7 @@ const Interest = () => {
             <div className="flex flex-col items-center mt-12">
                 {personalData.map((data, index) => (
                     <div key={data.id} className={`flex items-center border rounded-md  mb-4  p-2  w-full md:w-1/2 text-gray-800 cursor-pointer ${selectedBox === data.id ? 'border-golden' : 'border-gray-300'}`} onClick={() => handleBoxClick(data.id)}>
-                        <img src={data.image} alt="vector image" width={40} height={20} className='mr-5'/>
+                        <img src={data.image} alt="vector" width={40} height={20} className='mr-5'/>
                         {data.title}
                        
                     </div>
