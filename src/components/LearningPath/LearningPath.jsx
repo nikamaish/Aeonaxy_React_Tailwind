c4e6ec662dc75b5480771667a24ff9039293b5f7
aish <aishnikam31@gmail.com>
2024-04-02 23:31:20 +0530

    :heavy_plus_sign:fontawesome

--- src/components/LearningPath/LearningPath.jsx
+++ src/components/LearningPath/LearningPath.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import {faCircle} from '@fortawesome/free-regular-svg-icons';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const LearningPath = () => {
   return (
-    <div>
-      
+    <div className='flex justify-center items-center h-screen'>
+      <div className='flex flex-col items-center '>
+        <FontAwesomeIcon icon={faSpinner} className='text-yellow-500  mb-10' spin size="5x" />
+        <h3 className='whitespace-normal break-all'>Finding learning path recommendations for you based on your responses</h3>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LearningPath
+export default LearningPath;
