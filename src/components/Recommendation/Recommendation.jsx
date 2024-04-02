eba739c67128ad2dce290009f8a88adf993773c1
aish <aishnikam31@gmail.com>
2024-04-03 01:52:13 +0530

    :heavy_check_mark: recommnedation section done

--- src/components/Recommendation/Recommendation.jsx
+++ src/components/Recommendation/Recommendation.jsx
@@ -7,9 +7,11 @@ const Recommendation = () => {
       <h2 className="text-2xl font-bold text-gray-800 mt-2">Learning paths based on your answers</h2>
       <h3 className='text-gray-600 mt-2'>Choose one to get started. You can switch anytime</h3>
 
-
       <div className="flex flex-row justify-center mt-12">
-        <div className="w-1/4 flex items-center  p-4 border border-gray-500 mr-2">
+        <div className="w-1/4 relative p-4 border border-gray-500 rounded-md mr-2 flex">
+          {/* "Most Popular" tag */}
+          <div className="absolute -top-4 right-24 bg-yellow-500 text-black px-2 py-1 rounded-md">Most Popular</div>
+          
           <div>
             <h3 className="text-lg font-semibold">Foundational Math</h3>
             <p>Build your foundational skills in algebra, geometry, and probability.</p>
@@ -17,7 +19,7 @@ const Recommendation = () => {
           <img src="/assets/educational-tool.png" alt="vector" className="ml-4 w-24 h-24" />
         </div>
 
-        <div className="w-1/4  flex items-center  p-4 border border-gray-500 ml-2 ">
+        <div className="w-1/4 flex items-center p-4 border border-gray-500 rounded-md ml-2">
           <div>
             <h3 className="text-lg font-semibold">Mathematical Thinking</h3>
             <p>Build your foundational skills in algebra, geometry, and probability.</p>
@@ -25,8 +27,6 @@ const Recommendation = () => {
           <img src="/assets/mathematics.png" alt="vector" className="ml-4 w-24 h-24" />
         </div>
       </div>
-
-
     </div>
   );
 }
