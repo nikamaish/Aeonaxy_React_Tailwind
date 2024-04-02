8882ba1cd1ed45c9d2efcea5b866942ead87c2ba
aish <aishnikam31@gmail.com>
2024-04-02 16:32:21 +0530

    :art:interest images

--- src/components/Interest/Interest.jsx
+++ src/components/Interest/Interest.jsx
@@ -1,31 +1,33 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobe, faChartLine } from '@fortawesome/free-solid-svg-icons';
 
 const Interest = () => {
     const personalData = [
         {
             id: 1,
             title: "Learning specific skills to advance my career",
-            image: "/assets/vector.png",
+            image: "/assets/growth.png",
         },
         {
             id: 2,
             title: "Exploring new topics I'm interested in",
-            image: "/assets/vector.png",
+            image: "/assets/earth.png",
         },
         {
             id: 3,
             title: "Refreshing my math foundations",
-            image: "/assets/vector.png",
+            image: "/assets/calculating.png",
         },
         {
             id: 4,
             title: "Exercising my brain to stay sharp",
-            image: "/assets/vector.png",
+            image: "/assets/target.png",
         },
         {
             id: 5,
             title: "Something else",
-            image: "/assets/vector.png",
+            image: "/assets/eyes.png",
         }
     ];
 
@@ -42,9 +44,10 @@ const Interest = () => {
 
             <div className="flex flex-col items-center mt-12">
                 {personalData.map((data, index) => (
-                    <div key={data.id} className={`flex items-center border rounded-md  mb-4 w-full md:w-1/2 text-gray-800 cursor-pointer ${selectedBox === data.id ? 'border-golden' : 'border-gray-300'}`} onClick={() => handleBoxClick(data.id)}>
-                        <img src={data.image} alt="vector image" width={60} height={20} className='mr-5'/>
+                    <div key={data.id} className={`flex items-center border rounded-md  mb-4  p-2  w-full md:w-1/2 text-gray-800 cursor-pointer ${selectedBox === data.id ? 'border-golden' : 'border-gray-300'}`} onClick={() => handleBoxClick(data.id)}>
+                        <img src={data.image} alt="vector image" width={40} height={20} className='mr-5'/>
                         {data.title}
+                       
                     </div>
                 ))}
             </div>
