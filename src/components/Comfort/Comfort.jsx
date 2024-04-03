b2caa87d64688188c6b3cd2d47cec88df7d62e86
aish <aishnikam31@gmail.com>
2024-04-03 15:59:54 +0530

    :sparkles: comfort section done

--- src/components/Comfort/Comfort.jsx
+++ src/components/Comfort/Comfort.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Comfort = () => {
+  const comfortLevels = [
+    {
+      id: 0,
+      level: 'Introductory',
+      topic: 'Arithmetic',
+      equation: '/assets/equation1.png'
+    },
+    {
+      id: 1,
+      level: 'Foundational',
+      topic: 'Basic Algebra',
+      equation: '/assets/equation2.png'
+    },
+    {
+      id: 2,
+      level: 'Intermediate',
+      topic: 'Intermediate Algebra',
+      equation: '/assets/equation3.png'
+    },
+    {
+      id: 3,
+      level: 'Advanced',
+      topic: 'Calculus',
+      equation: '/assets/equation4.png'
+    }
+  ];
+
+  const [selectedBox, setSelectedBox] = useState(null);
+
+  const handleBoxClick = (id) => {
+    setSelectedBox(id);
+  };
+
   return (
-    <div>
-      
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-2xl font-bold text-gray-800 mt-2">What is your math comfort level?</h1>
+      <h3 className='text-gray-600 mt-2'>Choose the highest level you feel confident in &mdash;&nbsp;you can always adjust later.</h3>
+
+      <div className="flex gap-4 text-center mt-10 cursor-pointer">
+        {comfortLevels.map((level) => (
+         <div
+         key={level.id}
+         className={`flex-grow p-4 border rounded-lg ${selectedBox === level.id ? 'border-golden' : 'border-gray-300'}`}
+         onClick={() => handleBoxClick(level.id)}
+       >
+       
+            <img src={level.equation} alt="equation" className="h-36 w-36 object-contain" />
+            <h3 className="text-md">{level.topic}</h3>
+            <p className='text-gray-500'>{level.level}</p>
+          </div>
+        ))}
+      </div>
+
+      <button className={`bg-black text-white p-2 px-10 mb-2 rounded-md mt-8 ${selectedBox !== null ? '' : 'pointer-events-none opacity-50'}`} disabled={!selectedBox} ><Link to='/waypage'>Continue</Link></button>
     </div>
-  )
+  );
 }
 
-export default Comfort
+export default Comfort;
--- src/components/Recommendation/Recommendation.jsx
+++ src/components/Recommendation/Recommendation.jsx
@@ -7,8 +7,8 @@ const Recommendation = () => {
       <h2 className="text-2xl font-bold text-gray-800 mt-2">Learning paths based on your answers</h2>
       <h3 className='text-gray-600 mt-2'>Choose one to get started. You can switch anytime</h3>
 
-      <div className="flex flex-row justify-center mt-12">
-        <div className="w-1/4 relative p-4 border border-gray-500 rounded-md mr-2 flex">
+      <div className="flex flex-row justify-center mt-12 gap-4 ">
+        <div className="w-1/4 relative flex p-4 border border-gray-500 rounded-md  ">
           {/* "Most Popular" tag */}
           <div className="absolute -top-4 right-24 bg-yellow-500 text-black px-2 py-1 rounded-md">Most Popular</div>
           
@@ -19,7 +19,7 @@ const Recommendation = () => {
           <img src="/assets/educational-tool.png" alt="vector" className="ml-4 w-24 h-24" />
         </div>
 
-        <div className="w-1/4 flex items-center p-4 border border-gray-500 rounded-md ml-2">
+        <div className="w-1/4 flex items-center p-4 border border-gray-500 rounded-md ">
           <div>
             <h3 className="text-lg font-semibold">Mathematical Thinking</h3>
             <p>Build your foundational skills in algebra, geometry, and probability.</p>
